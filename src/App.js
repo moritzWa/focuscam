3180d2a1e42d16d4112a9f512fdd9b8e48e1aae9
Moritz Wallawitsch <wallawitsch@gmail.com>
2021-03-06 09:52:52 +0100

    formatting

--- src/App.js
+++ src/App.js
@@ -1,72 +1,96 @@
-import './App.css';
+import "./App.css";
 import React, { useRef, useEffect, useState } from "react";
 
-import Webcam from "react-webcam"
-import { calculatePredsAverage, getMaxClassName, takeSnapshot, models } from './utilities';
+import Webcam from "react-webcam";
+import {
+	calculatePredsAverage,
+	getMaxClassName,
+	takeSnapshot,
+	models,
+} from "./utilities";
 
 function App() {
-  const [monitorSetup, setMonitorSetup] = useState(models[0])
-  const [topPrediction, setTopPrediction] = useState("detecting...")
-  const [distractionDuration, setDistractionDuration] = useState(0)
+	const [monitorSetup, setMonitorSetup] = useState(models[0]);
+	const [topPrediction, setTopPrediction] = useState("detecting...");
+	const [distractionDuration, setDistractionDuration] = useState(0);
 
-  const webcamRef = useRef(null)
+	const webcamRef = useRef(null);
 
-  useEffect(() => {
-    let stackSize = 0;
-    let attached = true;
+	useEffect(() => {
+		let stackSize = 0;
+		let attached = true;
 
-    async function run() {
-      const video = webcamRef.current?.video;
+		async function run() {
+			const video = webcamRef.current?.video;
 
-      if (video?.readyState === HTMLMediaElement.HAVE_ENOUGH_DATA) {
-        await takeSnapshot(monitorSetup.name, video);
+			if (video?.readyState === HTMLMediaElement.HAVE_ENOUGH_DATA) {
+				await takeSnapshot(monitorSetup.name, video);
 
-        if (stackSize++ >= 50) {
-          console.log('update');
+				if (stackSize++ >= 50) {
+					console.log("update");
 
-          let topPreduction = getMaxClassName(calculatePredsAverage())
-          setTopPrediction(topPreduction);
+					let topPreduction = getMaxClassName(calculatePredsAverage());
+					setTopPrediction(topPreduction);
 
-          topPrediction == "distracted" ? setDistractionDuration(distractionDuration++)
+					if (topPrediction == "distracted") {
+						setDistractionDuration(distractionDuration++);
+					}
 
-          stackSize = 0;
-        }
+					stackSize = 0;
+				}
 
-        console.log(distractionDuration)
+				console.log(distractionDuration);
 
-        if (attached) {
-          setTimeout(run, 10);
-        }
-      } else {
-        if (attached) {
-          // try to run again after wait
-          setTimeout(run, 10);
-        }
-      }
-    }
+				if (attached) {
+					setTimeout(run, 10);
+				}
+			} else {
+				if (attached) {
+					// try to run again after wait
+					setTimeout(run, 10);
+				}
+			}
+		}
 
-    run();
+		run();
 
-    return () => attached = false;
-  });
+		return () => (attached = false);
+	});
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Focuscam ={" "} {topPrediction}</h1>
-        <p>A web app that tracks and helps you improv your focus using computer vision.
-      </p>
-        <div>
-          Choose your Monitor Setup:
-        <select value={monitorSetup.name} onChange={e => setMonitorSetup(models.find(value => value.name === e.currentTarget.value))}>
-            <option value='laptop'>Laptop Webcam</option>
-            <option value='external'>Laptop Webcam with external Monitor</option>
-          </select>
-        </div>
-      </header>
-      <Webcam ref={webcamRef} audio={false} mirrored={false} onUserMedia={() => webcamRef.current.audio = false} style={{ "display": 'none' }} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Focuscam = {topPrediction}</h1>
+				<p>
+					A web app that tracks and helps you improv your focus using computer
+					vision.
+				</p>
+				<div>
+					Choose your Monitor Setup:
+					<select
+						value={monitorSetup.name}
+						onChange={(e) =>
+							setMonitorSetup(
+								models.find((value) => value.name === e.currentTarget.value)
+							)
+						}
+					>
+						<option value="laptop">Laptop Webcam</option>
+						<option value="external">
+							Laptop Webcam with external Monitor
+						</option>
+					</select>
+				</div>
+			</header>
+			<Webcam
+				ref={webcamRef}
+				audio={false}
+				mirrored={false}
+				onUserMedia={() => (webcamRef.current.audio = false)}
+				style={{ display: "none" }}
+			/>
+		</div>
+	);
 }
 
 export default App;
--- src/utilities.js
+++ src/utilities.js
@@ -1,78 +1,79 @@
-import * as tmImage from '@teachablemachine/image';
+import * as tmImage from "@teachablemachine/image";
 
-// Model Utilities 
+// Model Utilities
 
 export const models = [
-  {
-    name: "laptop",
-    modelUrl: "https://teachablemachine.withgoogle.com/models/34tgAu-tp/",
-  },
-  {
-    name: "external",
-    modelUrl: "https://teachablemachine.withgoogle.com/models/aDUyx7SWA/",
-  }
-]
+	{
+		name: "laptop",
+		modelUrl: "https://teachablemachine.withgoogle.com/models/34tgAu-tp/",
+	},
+	{
+		name: "external",
+		modelUrl: "https://teachablemachine.withgoogle.com/models/aDUyx7SWA/",
+	},
+];
 
 const modelCache = {};
 
 async function loadModel(modelUrl) {
-  const modelURL = modelUrl + "model.json";
-  const metadataURL = modelUrl + "metadata.json";
+	const modelURL = modelUrl + "model.json";
+	const metadataURL = modelUrl + "metadata.json";
 
-  return await tmImage.load(modelURL, metadataURL);
+	return await tmImage.load(modelURL, metadataURL);
 }
 
 async function getModel(modelName) {
-  if (modelName in modelCache) {
-    return modelCache[modelName];
-  }
+	if (modelName in modelCache) {
+		return modelCache[modelName];
+	}
 
-  return modelCache[modelName] = await loadModel(models.find(element => element.name === modelName).modelUrl);
+	return (modelCache[modelName] = await loadModel(
+		models.find((element) => element.name === modelName).modelUrl
+	));
 }
-
-
 
 // Prediction Utilities
 
 let predictions = [];
 
 export async function takeSnapshot(modelName, video) {
-  const model = await getModel(modelName);
-  const prediction = await model.predict(video);
+	const model = await getModel(modelName);
+	const prediction = await model.predict(video);
 
-  predictions.push(...prediction);
+	predictions.push(...prediction);
 }
 
 export function calculatePredsAverage() {
-  if (!predictions.length)
-    return null;
+	if (!predictions.length) return null;
 
-  // accumulate results
-  const result = {};
-  predictions.forEach(({ className, probability }) => result[className] = (result[className] ?? 0) + probability);
+	// accumulate results
+	const result = {};
+	predictions.forEach(
+		({ className, probability }) =>
+			(result[className] = (result[className] ?? 0) + probability)
+	);
 
-  predictions = [];
+	predictions = [];
 
-  // temporarely turn of 'fingers' since the preds are off here 
-  result.fingers = 0
+	// temporarely turn of 'fingers' since the preds are off here
+	result.fingers = 0;
 
-  return result
+	return result;
 }
 
 export function getMaxClassName(result) {
-  let maxProbability = 0;
-  let maxClassName;
-  Object.entries(result)
-    .forEach(([className, probability]) => {
-      if (probability > maxProbability) {
-        maxProbability = probability;
-        maxClassName = className;
-      }
-    });
+	let maxProbability = 0;
+	let maxClassName;
+	Object.entries(result).forEach(([className, probability]) => {
+		if (probability > maxProbability) {
+			maxProbability = probability;
+			maxClassName = className;
+		}
+	});
 
-  return maxClassName
+	return maxClassName;
 }
 
 // UI Utilities
 
-export function
+//export function
